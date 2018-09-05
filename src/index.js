cd20b17af2a06ed6aaaa9ac2547525a4cc50b179
日天 <gaoryrt@users.noreply.github.com>
2018-09-05 18:08:34 +0800

    🚧 put utils into utils.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,51 @@
-import { isStr, isNum, str2RGBAArr } from './utils'
+import { isStr, isNum, h2n, d2n, agl2trn, str2num, HSLA2RGB } from './utils'
+
+const str2RGBAArr = str => {
+  const _mArr = [
+    {
+      reg: /^#([0-9a-f]{1})([0-9a-f]{1})([0-9a-f]{1})$/,
+      fn: mt => {
+        const A = mt[1]
+        const B = mt[2]
+        const C = mt[3]
+        return [h2n(A + A), h2n(B + B), h2n(C + C), 1]
+      }
+    },
+    {
+      reg: /^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/,
+      fn: mt => [h2n(mt[1]), h2n(mt[2]), h2n(mt[3]), 1]
+    },
+    {
+      reg: /^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/,
+      fn: mt => [h2n(mt[1]), h2n(mt[2]), h2n(mt[3]), h2n(mt[4])]
+    },
+    {
+      reg: /^rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)$/,
+      fn: mt => [d2n(mt[1]), d2n(mt[2]), d2n(mt[3]), 1]
+    },
+    {
+      reg: /^rgba\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3}),\s*(\d+(?:\.\d+)?|\.\d+)\)$/,
+      fn: mt => [d2n(mt[1]), d2n(mt[2]), d2n(mt[3]), parseFloat(mt[4])]
+    },
+    {
+      reg: /^hsl\((.+?),\s*(.+?),\s*(.+?)\)$/,
+      fn: mt => HSLA2RGB(agl2trn(mt[1]) % 1, str2num(mt[2]), str2num(mt[3]), 1)
+    },
+    {
+      reg: /^hsla\((.+?),\s*(.+?),\s*(.+?),\s*(\d+(?:\.\d+)?|\.\d+)\)$/,
+      fn: mt => HSLA2RGB(agl2trn(mt[1]) % 1, str2num(mt[2]), str2num(mt[3]), parseFloat(mt[4]))
+    }
+  ]
+  for (let o of _mArr) {
+    let m = o.reg.exec(str)
+    if (m) return o.fn(m)
+  }
+}
 
 function rtn(...args) {
-  const invalid = () => {throw new Error('invalid input: ' + args)}
+  const invalid = () => {
+    throw new Error('invalid input: ' + args)
+  }
   const argLen = args.length
   let RGBA = []
   if (argLen < 1) throw new Error('no input detected')
@@ -10,8 +54,7 @@ function rtn(...args) {
     if (isStr(arg0)) {
       RGBA = str2RGBAArr(arg0)
     } else if (isNum(arg0)) {
-      if (arg0 > 0xffffff) invalid()
-      
+      if (arg0 > 0xffffff || arg0 < 0) invalid()
     } else invalid()
     console.log('RGBAArr', RGBA)
   }
@@ -19,7 +62,11 @@ function rtn(...args) {
 }
 
 // export default rtn
+rtn('#fff')
+rtn('#ffffff')
+rtn('rgb(1, 1, 1)')
+rtn('rgba(22, 22, 22, .8)')
+rtn('hsl(11, 89%, 89%)')
+rtn('hsla(11, 89%, 89%, .8)')
 
-rtn('hsl(75,.77,.77)')
-rtn('#DBF297')
-
+console.log((0x0ff).toString(16))
--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,14 @@
 const isStr = a => Object.prototype.toString.call(a) === '[object String]'
 const isNum = a => !isNaN(parseFloat(a)) && isFinite(a)
-
-const _h2n = hex => parseInt(hex, 16)
-const _d2n = dec => parseInt(dec, 10)
+const h2n = hex => parseInt(hex, 16)
+const d2n = dec => parseInt(dec, 10)
 const agl2trn = str => {
   const mt = /^(\d+(?:\.\d+)?|\.\d+)(.*?)$/.exec(str)
   const deObj = {
-    'deg': 360,
-    'rad': 2 * Math.PI,
-    'grad': 400,
-    'turn': 1
+    deg: 360,
+    rad: 2 * Math.PI,
+    grad: 400,
+    turn: 1
   }
   if (!mt) throw new Error(str + ' is not an angle')
   if (!mt[2]) return parseFloat(mt[1]) / 360
@@ -26,9 +25,9 @@ const str2num = str => {
   return parseFloat(mt[1]) / deObj[mt[2]]
 }
 
-const _HSLA2RGB = (H, S, L, A) => {
+const HSLA2RGB = (H, S, L, A) => {
   // http://www.easyrgb.com/en/math.php
-  const __Hue2RGB = (v1, v2, vH) => {
+  const Hue2RGB = (v1, v2, vH) => {
     if (vH < 0) vH += 1
     if (vH > 1) vH -= 1
     if (6 * vH < 1) return v1 + (v2 - v1) * 6 * vH
@@ -45,53 +44,10 @@ const _HSLA2RGB = (H, S, L, A) => {
     if (L < 0.5) t1 = L * (1 + S)
     else t1 = L + S - S * L
     const t0 = 2 * L - t1
-    R = 255 * __Hue2RGB(t0, t1, H + 1 / 3)
-    G = 255 * __Hue2RGB(t0, t1, H)
-    B = 255 * __Hue2RGB(t0, t1, H - 1 / 3)
+    R = 255 * Hue2RGB(t0, t1, H + 1 / 3)
+    G = 255 * Hue2RGB(t0, t1, H)
+    B = 255 * Hue2RGB(t0, t1, H - 1 / 3)
   }
   return [R, G, B, A]
 }
-
-const str2RGBAArr = str => {
-  const _mArr = [
-    {
-      reg: /^#([0-9a-f]{1})([0-9a-f]{1})([0-9a-f]{1})$/,
-      fn: mt => {
-        const A = mt[1]
-        const B = mt[2]
-        const C = mt[3]
-        return [_h2n(A + A), _h2n(B + B), _h2n(C + C), 1]
-      }
-    },
-    {
-      reg: /^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/,
-      fn: mt => [_h2n(mt[1]), _h2n(mt[2]), _h2n(mt[3]), 1]
-    },
-    {
-      reg: /^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/,
-      fn: mt => [_h2n(mt[1]), _h2n(mt[2]), _h2n(mt[3]), _h2n(mt[4])]
-    },
-    {
-      reg: /^rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)$/,
-      fn: mt => [_d2n(mt[1]), _d2n(mt[2]), _d2n(mt[3]), 1]
-    },
-    {
-      reg: /^rgba\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3}),\s*(\d+(?:\.\d+)?|\.\d+)\)$/,
-      fn: mt => [_d2n(mt[1]), _d2n(mt[2]), _d2n(mt[3]), parseFloat(mt[4])]
-    },
-    {
-      reg: /^hsl\((.+?),\s*(.+?),\s*(.+?)\)$/,
-      fn: mt => _HSLA2RGB(agl2trn(mt[1]) % 1, str2num(mt[2]), str2num(mt[3]), 1)
-    },
-    {
-      reg: /^hsla\((.+?),\s*(.+?),\s*(.+?),\s*(\d+(?:\.\d+)?|\.\d+)\)$/,
-      fn: mt => _HSLA2RGB(agl2trn(mt[1]) % 1, str2num(mt[2]), str2num(mt[3]), parseFloat(mt[4]))
-    }
-  ]
-  for (let o of _mArr) {
-    let m = o.reg.exec(str)
-    if (m) return o.fn(m)
-  }
-}
-
-export { isStr, isNum, str2RGBAArr, startWith }
+export { isStr, isNum, h2n, d2n, agl2trn, str2num, HSLA2RGB }

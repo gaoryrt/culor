bb8f0fa537db2087fce7cfc3ee74a9dd0be14dea
日天 <gaoryrt@users.noreply.github.com>
2018-09-06 11:08:17 +0800

    🚧 comments

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { isStr, isNum, h2n, d2n, agl2trn, str2num, HSLA2RGB, num2str } from './utils'
+import { isStr, isNum, h2n, d2n, agl2trn, HSLA2RGB, num2str, per2dec } from './utils'
 
 const str2RGBAArr = str => {
   const _mArr = [
@@ -16,12 +16,12 @@ const str2RGBAArr = str => {
       fn: mt => [h2n(mt[1]), h2n(mt[2]), h2n(mt[3]), 1]
     },
     {
-      reg: /^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/,
-      fn: mt => [h2n(mt[1]), h2n(mt[2]), h2n(mt[3]), h2n(mt[4])]
-    },
-    {
       reg: /^rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)$/,
       fn: mt => [d2n(mt[1]), d2n(mt[2]), d2n(mt[3]), 1]
+    },
+    {
+      reg: /^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/,
+      fn: mt => [h2n(mt[1]), h2n(mt[2]), h2n(mt[3]), h2n(mt[4])]
     },
     {
       reg: /^rgba\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3}),\s*(\d+(?:\.\d+)?|\.\d+)\)$/,
@@ -29,11 +29,11 @@ const str2RGBAArr = str => {
     },
     {
       reg: /^hsl\((.+?),\s*(.+?),\s*(.+?)\)$/,
-      fn: mt => HSLA2RGB(agl2trn(mt[1]) % 1, str2num(mt[2]), str2num(mt[3]), 1)
+      fn: mt => HSLA2RGB(agl2trn(mt[1]) % 1, per2dec(mt[2]) || d2n(mt[2]), per2dec(mt[3]) || d2n(mt[3]), 1)
     },
     {
       reg: /^hsla\((.+?),\s*(.+?),\s*(.+?),\s*(\d+(?:\.\d+)?|\.\d+)\)$/,
-      fn: mt => HSLA2RGB(agl2trn(mt[1]) % 1, str2num(mt[2]), str2num(mt[3]), parseFloat(mt[4]))
+      fn: mt => HSLA2RGB(agl2trn(mt[1]) % 1, per2dec(mt[2]) || d2n(mt[2]), per2dec(mt[3]) || d2n(mt[3]), parseFloat(mt[4]))
     }
   ]
   for (let o of _mArr) {
@@ -42,7 +42,7 @@ const str2RGBAArr = str => {
   }
 }
 
-function rtn(...args) {
+function roloc(...args) {
   const invalid = () => {
     throw new Error('invalid input: ' + args)
   }
@@ -59,14 +59,14 @@ function rtn(...args) {
     } else invalid()
     console.log('RGBAArr', RGBA)
   }
-  if (argLen === 2) invalid()
+  if (argLen >= 2) invalid()
+  return RGBA
 }
 
-// export default rtn
-rtn('#fff')
-rtn('#ffffff')
-rtn('rgb(1, 1, 1)')
-rtn('rgba(22, 22, 22, .8)')
-rtn('hsl(11, 89%, 89%)')
-rtn('hsla(11, 89%, 89%, .8)')
-rtn(0xfff)
+// export default roloc
+roloc('#ffffff')
+roloc('rgb(1, 1, 1)')
+roloc('rgba(22, 22, 22, .8)')
+roloc('hsl(11, 89%, 89%)')
+roloc('hsla(11, 89%, 89%, .8)')
+console.log(roloc(0xffffff))
--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,13 @@ const isStr = a => Object.prototype.toString.call(a) === '[object String]'
 const isNum = a => !isNaN(parseFloat(a)) && isFinite(a)
 const h2n = hex => parseInt(hex, 16)
 const d2n = dec => parseInt(dec, 10)
+
+/**
+ * number to HexString
+ * @param {Number} hex
+ * 0x444 -> '#000444'
+ * 1092  -> '#000444'
+ */
 const num2str = hex => {
   if (hex <= 0xfff) console.warn('three-digit form hex color may cause problem (0xfff === 0x000fff)')
   let rtn = hex.toString(16)
@@ -9,6 +16,15 @@ const num2str = hex => {
   return '#' + rtn
 }
 
+/**
+ * angleString to turn
+ * @param {String} str angle string
+ * '360'       -> 1
+ * '360deg'    -> 1
+ * '3.1416rad' -> 0.4984096197865795
+ * '400grad'   -> 1
+ * '1turn'     -> 1
+ */
 const agl2trn = str => {
   const mt = /^(\d+(?:\.\d+)?|\.\d+)(.*?)$/.exec(str)
   const deObj = {
@@ -22,18 +38,27 @@ const agl2trn = str => {
   return parseFloat(mt[1]) / deObj[mt[2].toLowerCase()]
 }
 
-const str2num = str => {
-  const mt = /^(\d+(?:\.\d+)?|\.\d+)(.*?)$/.exec(str)
-  const deObj = {
-    '': 1,
-    '%': 100
-  }
-  if (!mt) return NaN
-  return parseFloat(mt[1]) / deObj[mt[2]]
+/**
+ * percentString to decimalNumber
+ * @param {String} str
+ * '123.4%' -> 1.234
+ * '200%' -> 2
+ * '200' -> false
+ */
+const per2dec = str => {
+  const mt = /^(\d+(?:\.\d+)?|\.\d+)%$/.exec(str)
+  if (!mt) return false
+  return mt[1] / 100
 }
 
+/**
+ * http://www.easyrgb.com/en/math.php
+ * @param {Number} H [0, 1)
+ * @param {Number} S [0, 1)
+ * @param {Number} L [0, 1)
+ * @param {Number} A [0, 1)
+ */
 const HSLA2RGB = (H, S, L, A) => {
-  // http://www.easyrgb.com/en/math.php
   const Hue2RGB = (v1, v2, vH) => {
     if (vH < 0) vH += 1
     if (vH > 1) vH -= 1
@@ -58,4 +83,4 @@ const HSLA2RGB = (H, S, L, A) => {
   return [R, G, B, A]
 }
 
-export { isStr, isNum, h2n, d2n, agl2trn, str2num, HSLA2RGB, num2str }
+export { isStr, isNum, h2n, d2n, agl2trn, HSLA2RGB, num2str, per2dec }
